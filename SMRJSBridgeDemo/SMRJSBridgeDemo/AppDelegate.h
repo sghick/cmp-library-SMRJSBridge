8d748d79faca475dbaf658189782b34a0434ae3b
丁治文 <sghick@sghick.local>
2018-07-17 23:46:47 +0800

    new project

---
+++ SMRJSBridgeDemo/SMRJSBridgeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SMRJSBridgeDemo
+//
+//  Created by 丁治文 on 2018/7/17.
+//  Copyright © 2018年 sumrise.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
